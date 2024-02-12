a43333202f5ade2829e38e80acdcd33b59d27840
Nguyen Hoang <nguyenhoangcit@gmail.com>
2024-02-12 19:00:19 +0700

    Initial commit

---
+++ js/header.js
@@ -0,0 +1,8 @@
+const menuBar = document.querySelector(".header__menu-icon")
+const navBar = document.querySelector(".navbar")
+
+menuBar.addEventListener("click", function() {
+    this.querySelector(".fa-solid.fa-bars").classList.toggle("d-none");
+    this.querySelector(".fa-solid.fa-xmark").classList.toggle("d-none");
+    navBar.classList.toggle("d-block")
+})
