9796450a7d267c62b863144c947c1b15b64224a0
Nguyen Hoang <nguyenhoangcit@gmail.com>
2024-02-19 04:22:06 +0700

    up

--- js/header.js
+++ js/header.js
@@ -4,5 +4,5 @@ const navBar = document.querySelector(".navbar")
 menuBar.addEventListener("click", function() {
     this.querySelector(".fa-solid.fa-bars").classList.toggle("d-none");
     this.querySelector(".fa-solid.fa-xmark").classList.toggle("d-none");
-    navBar.classList.toggle("d-block")
+    navBar.classList.toggle("d-block");
 })
