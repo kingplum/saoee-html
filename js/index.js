6bd5cb27c168ee30b241ebf78f7f3267a4f6362a
Nguyen Hoang <nguyenhoangcit@gmail.com>
2024-02-19 06:19:48 +0700

    ok

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($){
-    $('.carousel__list.js-slider').slick({
+  if($(window).width() <= 991) {
+    $('.section__project .project__carousel .carousel__list').slick({
       infinite: true,
       speed: 300,
       slidesToShow: 3,
@@ -17,7 +18,7 @@ jQuery(document).ready(function($){
           }
         },
         {
-          breakpoint: 600,
+          breakpoint: 991,
           settings: {
             slidesToShow: 2,
             slidesToScroll: 2
@@ -35,22 +36,59 @@ jQuery(document).ready(function($){
         // instead of a settings object
       ]
     });
-    $('.slider').slick({
-      autoplaySpeed: 1000,
-      pauseOnHover: true,
-      arrows: false,
-      dots: false,
-      infinite: false,
-      fade: true,
-      asNavFor: '.slider-nav'
-    });
-    $('.slider-nav').slick({
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      arrows: false,
-      dots: false,
-      infinite: false,
-      focusOnSelect: true,
-      asNavFor: '.slider',
-    });
+  }
+  $('.carousel__list.js-slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button class="btn--controll slick-prev"><i class="fa-solid fa-arrow-left"></i></button>',
+    nextArrow: '<button class="btn--controll slick-next"><i class="fa-solid fa-arrow-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  $('.slider').slick({
+    autoplaySpeed: 1000,
+    pauseOnHover: true,
+    arrows: false,
+    dots: false,
+    infinite: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    infinite: false,
+    focusOnSelect: true,
+    asNavFor: '.slider',
+  });
 });
